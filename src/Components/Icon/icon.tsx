b4d7613876a4213321c2def05b121a4a1d1add7e
ENTITY514 <entity514@mail.ru>
2023-02-18 10:20:15 +0600

    graphics component update, tint change, optimization

--- src/Components/Icon/icon.tsx
+++ src/Components/Icon/icon.tsx
@@ -6,8 +6,6 @@ interface IIconProps {
 }
 
 export const Icon: React.FC<IIconProps> = ({ url, is_active = false }) => {
-    console.log(is_active);
-    
     return (
         <div className={is_active ? style.active_container : style.container} >
             <div className={style.img} style={{ backgroundImage: "url(" + url + ")" }}></div>
--- src/Pages/engine/engine/Classes/Objects/components/Graphic/graphic_view.tsx
+++ src/Pages/engine/engine/Classes/Objects/components/Graphic/graphic_view.tsx
@@ -9,11 +9,20 @@ interface GraphicViewProps {
 
 export const GraphicView: React.FC<GraphicViewProps> = ({ component }) => {
     const alpha_inp_ref = React.useRef<HTMLInputElement>(null)
+    const tint_inp_ref = React.useRef<HTMLInputElement>(null)
+    const getTintToHex = React.useMemo(() => { return "#" + component.getTint().toString(16) }, [component.object.sprite.tint])
     return (
         <div className={style.container}>
             <div className={style.name}>Graphics</div>
             <div className={style.textureName}>Texture file: {component.getTextureFile() ? component.getTextureFile()?.name : "None File"}</div>
-            <div className={style.tint}>Tint: {component.getTint()}</div>
+            <div className={style.tint}>Tint: {getTintToHex}</div><input
+                ref={tint_inp_ref}
+                value={getTintToHex}
+                type="color"
+                onChange={() => {
+                    component.changeTint(Number("0x" + tint_inp_ref.current?.value.slice(1)))
+                }}
+            />
             <div className={style.opacity}>Alpha: {component.getOpacity()}</div>
             <input
                 ref={alpha_inp_ref}
